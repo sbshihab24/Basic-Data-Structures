9938e544c1059248caafb36f407c7881c08416c5
Mehedi Hasan Shihab <sbshihab2000@gmail.com>
2025-06-23 21:41:17 +0600

    Initial commit with full structured folders

---
+++ Week-1/Module 1/linear_complexity_O_n.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()        // overall time complexity O(n)
+{
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n;i++)
+    {
+        cout << i << " " ;  
+    }
+    return 0;
+}
